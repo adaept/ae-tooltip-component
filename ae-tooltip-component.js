1221203ee97d4eb26327beb2010f78f4d3dd6074
peterennisrda <peterennis@resourcedevelopment.net>
2018-12-08 17:50:01 -0800

    chore () tidy code

--- ae-tooltip-component.js
+++ ae-tooltip-component.js
@@ -14,6 +14,7 @@ class TooltipComponent extends HTMLElement {
         tooltipHint.addEventListener('mouseenter', this._showTooltipHint.bind(this));
         tooltipHint.addEventListener('mouseleave', this._hideTooltipHint.bind(this));
         this.appendChild(tooltipHint);
+        this.style.position = 'relative';
     }
 
     _showTooltipHint() {
@@ -21,11 +22,12 @@ class TooltipComponent extends HTMLElement {
         this._tooltipContainer.textContent = this._tooltipText;
         this._tooltipContainer.style.backgroundColor = 'black';
         this._tooltipContainer.style.color = 'white';
+        this.style.position = 'absolute';
+        this.style.zIndex = '10';
         this.appendChild(this._tooltipContainer);
     }
 
     _hideTooltipHint() {
-        console.log('_hideTooltipHint')
         this.removeChild(this._tooltipContainer);
     }
 }
