6cd2c20f41f6d354ce4436702120bbf349821afc
peterennis <peterennis@yahoo.com>
2018-12-09 10:19:52 -0800

    refactor () move style to innerhtml

--- ae-tooltip-component.js
+++ ae-tooltip-component.js
@@ -5,6 +5,14 @@ class TooltipComponent extends HTMLElement {
         this._tooltipText = "Default value for tooltipText"
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = `
+            <style>
+                div {
+                    background-color: black;
+                    color: white;
+                    position: absolute;
+                    z-index: 10;
+                }
+            </style>
             <span> (?)</span>
             <slot></slot><span> (?)</span>
         `;
@@ -24,10 +32,6 @@ class TooltipComponent extends HTMLElement {
     _showTooltipHint() {
         this._tooltipContainer = document.createElement('div');
         this._tooltipContainer.textContent = this._tooltipText;
-        this._tooltipContainer.style.backgroundColor = 'black';
-        this._tooltipContainer.style.color = 'white';
-        this.style.position = 'absolute';
-        this.style.zIndex = '10';
         this.shadowRoot.appendChild(this._tooltipContainer);
     }
 
